3ee5754c904552e02b76333e5d0c943d9ce5f4e4
utelagazkodas <vargamiki53@gmail.com>
2025-12-19 07:54:14 +0100

    end me

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,23 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <NTPClient.h>
+#include <Arduino.h>
 
-const char* ssid     = "NAME";
-const char* password = "PASSWORD";
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000); // 0 = UTC offset in seconds, 60000 = update interval in ms
+#define SWITCH_PIN D1
+#define OUTPUT_PIN D2
 
 void setup() {
+  pinMode(SWITCH_PIN, INPUT_PULLUP); // internal pull-up
+  pinMode(OUTPUT_PIN, OUTPUT);
+
+  digitalWrite(OUTPUT_PIN, LOW); // default off
+
   Serial.begin(9600);
-  WiFi.begin(ssid, password);
-
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWi-Fi connected");
-
-  timeClient.begin(); // start NTP client
 }
 
 void loop() {
-  timeClient.update(); // request current time from server
-
-  unsigned long epochTime = timeClient.getEpochTime(); // seconds since 1970
-  Serial.print("Epoch time: ");
-  Serial.println(epochTime);
-
-  Serial.print("Formatted time: ");
-  Serial.println(timeClient.getFormattedTime()); // HH:MM:SS
-
-  delay(1000);
+  // Switch pressed = LOW (because of pull-up)
+  if (digitalRead(SWITCH_PIN) == LOW) {
+    digitalWrite(OUTPUT_PIN, LOW);
+  } else {
+    digitalWrite(OUTPUT_PIN, HIGH);
+    Serial.println("this bitch is pressed");
+  }
 }
